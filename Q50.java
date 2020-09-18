9ed17bdd41a9177527182eeea8c056fbd76b198b
garudritesh <65205737+garudritesh@users.noreply.github.com>
2020-09-18 22:47:55 +0530

    Add files via upload

---
+++ Q50.java
@@ -0,0 +1,17 @@
+    
+  public class Q50{
+	public static void main(String args[]){
+		
+             
+           int arr[] = new int[3];
+         
+
+                      System.out.print(arr[0]);
+                
+                                           
+           
+ 
+	}
+}
+
+
