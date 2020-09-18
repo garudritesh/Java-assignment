073d26587b65411871af506f6e334c8318201739
garudritesh <65205737+garudritesh@users.noreply.github.com>
2020-09-19 01:14:05 +0530

    Update Q22.java

--- Q22.java
+++ Q22.java
@@ -1,17 +1,36 @@
-     import java.util.*; 
-  public class Q22{
-	public static void main(String args[]){
-		Scanner sc = new Scanner(System.in);
-             
-           int arr[] = new int[0];
-         
+import java.util.Scanner;
+class Q22
+{
+  public static void main(String args[])
+  {
+   Scanner sc=new Scanner(System.in);
+   System.out.println("Enter N number: ");
+    int num=sc.nextInt();
+    int arr[]=new int[num];
+   int temp=0;
+   System.out.println("Enter elements in array: ");
+    for(int i=0;i<num;i++)
+   {
+      arr[i]=sc.nextInt();
+  }
+   for(int i=0;i<num-1;i++)
+   {
+     for(int j=i+1;j<num;j++) 
+     {
+       if(arr[i] > arr[j])
+        {
+          temp=arr[i];
+           arr[i]=arr[j]; 
+          arr[j]=temp; 
+        }
+     }
+  }
+ System.out.print("Enter elements in Asending order: ");
+  for(int i=0;i<num;i++)
+   {
+      System.out.print(arr[i]+" ");
+  }
 
-                      System.out.print(arr.length);
-                
-                                           
-           
- 
-	}
+   }
 }
-
 
